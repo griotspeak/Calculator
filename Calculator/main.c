72e95327eb799501134fd2ebc1818b21131459b2
TJ Usiyan <griotspeak@gmail.com>
2016-08-08 16:04:57 -0400

    Initial Commit

---
+++ Calculator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Calculator
+//
+//  Created by TJ Usiyan on 2016/08/08.
+//  Copyright © 2016 Buttons and Lights LLC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
